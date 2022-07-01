3c832c5b4990fc39c5e9bb92faad7a9523354b99
shanmukeshwar03 <shanmukeshwar03@gmail.com>
2022-07-01 10:43:07 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# URL to PNG - API
---
+++ src/index.js
@@ -0,0 +1,57 @@
+import "dotenv/config";
+import puppeteer from "puppeteer";
+import FormData from "form-data";
+import axios from "axios";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", async (req, res) => {
+  const { v_height, v_width, height, width, deviceScaleFactor, x, y, url } =
+    req.body;
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url, {
+    waitUntil: "networkidle2",
+  });
+  await page.setViewport({
+    height: v_height,
+    width: v_width,
+    deviceScaleFactor,
+  });
+  const screenshot = await page.screenshot({
+    type: "png",
+    clip: {
+      x,
+      y,
+      height,
+      width,
+    },
+    encoding: "base64",
+  });
+  const base64 = screenshot.toString("base64");
+
+  const data = new FormData();
+  data.append("image", base64);
+  var config = {
+    method: "post",
+    url: `https://api.imgbb.com/1/upload?key=${process.env.API_KEY}`,
+    headers: {
+      ...data.getHeaders(),
+    },
+    data: data,
+  };
+
+  try {
+    const response = await axios(config);
+    res.send(response.data.data.url);
+  } catch (error) {
+    res.send("Something went wrong");
+  }
+  await browser.close();
+});
+
+app.listen(8000);
