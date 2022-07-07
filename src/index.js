52feb7cef6427cd44296ab3a36d6425ae059ec92
shanmukeshwar03 <shanmukeshwar03@gmail.com>
2022-07-07 21:17:52 +0530

    test jenkins trigger

--- src/index.js
+++ src/index.js
@@ -12,8 +12,5 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/screenshot", screenshotRouter);
 app.use("/horoscope", horoscopeRouter);
-app.get("/test", (req, res) => {
-  res.send("Hello World!!");
-});
 
 app.listen(8000);
--- src/routes/horoscope.routes.js
+++ src/routes/horoscope.routes.js
@@ -3,6 +3,9 @@ import { scrapeHoroscope } from "../controllers/horoscope.controller.js";
 
 const router = Router();
 
-router.get("/", scrapeHoroscope);
+// router.get("/", scrapeHoroscope);
+router.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 export default router;
